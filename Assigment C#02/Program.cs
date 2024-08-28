5a5768db7223afc183134cdf8104fd59a77d8009
user <user@DESKTOP-KAMUS30>
2024-08-28 12:29:42 +0300

    Add project files.

---
+++ Assigment C#02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigment_C_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
