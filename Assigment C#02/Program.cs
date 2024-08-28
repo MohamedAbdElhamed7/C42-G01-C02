8a1d0d1d0a7d635a20fa23b26d1cd7fe33429378
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-08-28 13:51:17 +0300

    Question 1

--- Assigment C#02/Program.cs
+++ Assigment C#02/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1.Write a program that allows the user to enter a number then print it.
+
+            Console.WriteLine("Enter The Number ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($" The Number is :{number} ");
+         // Q1 Done 
+            #endregion
         }
     }
 }
