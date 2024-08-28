b0ac37abb4754ce75d9d9db699fd20c207758ad4
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-08-28 15:50:55 +0300

    Question 2 Done

--- Assigment C#02/Program.cs
+++ Assigment C#02/Program.cs
@@ -12,8 +12,29 @@
             //Console.WriteLine($" The Number is :{number} ");
             // Q1 Done 
             #endregion
+
+            #region Q2
+            //Q2.Write C# program that Assigning one value type variable to another
+            //and modifying the value of one variable and mention what will happen
+
+            //int x = 5;
+            //int y = 8;
+
+            //x = y;
+            //so x will be equals to 8
+
+            //y = 3;
+            // so y will be equal 3 but x will still equals to 8
+            // becouse x = y thats mean that x equals to the  value of y (8) not equal to the y
+            // so x will not change from 8 to 3 
+
+            //Console.WriteLine($"The value of x is : {x}");
+            //Console.WriteLine($"The value of y is : {y}");
+
+
+            #endregion
         }
-        
-   
+
+
     }
 }
