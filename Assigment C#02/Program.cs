e3790d464b369817910e03eeec8685068d28b51b
MohamedAbdElhamed7 <mohamedabdalhamed845@gmail.com>
2024-08-28 15:06:31 +0300

    Selved Q1

--- Assigment C#02/Program.cs
+++ Assigment C#02/Program.cs
@@ -7,11 +7,13 @@
             #region Q1
             //1.Write a program that allows the user to enter a number then print it.
 
-            Console.WriteLine("Enter The Number ");
-            int number = int.Parse(Console.ReadLine());
-            Console.WriteLine($" The Number is :{number} ");
-         // Q1 Done 
+            //Console.WriteLine("Enter The Number ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine($" The Number is :{number} ");
+            // Q1 Done 
             #endregion
         }
+        
+   
     }
 }
